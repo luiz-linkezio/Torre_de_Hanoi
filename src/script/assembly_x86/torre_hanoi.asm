0af5fb07dbec06068f087886c8f7aef3b5a0f58a
José Ronaldo <dev.joseronaldo@gmail.com>
2024-02-03 14:59:47 -0300

    feat: add print input

--- src/script/assembly_x86/torre_hanoi.asm
+++ src/script/assembly_x86/torre_hanoi.asm
@@ -8,6 +8,30 @@ _start:
     mov ecx, entrada ; Ponteiro para o buffer de entrada
     call ler_3_2 ; Leitura do input do usuário que terá 3 bytes, mas só serão lidos 2
 
+    call printar_ecx
+
+printar_ecx: ; Imprimir a string que está em ecx
+    mov eax, 4               ; Número da chamada de sistema para imprimir
+    mov ebx, 1               ; Descritor de arquivo (stdout)
+    mov edx, 1               ; Tamanho do caractere a ser impresso
+    int 0x80                 ; Chamar a interrupção do sistema
+    ret                      ; Retorno
+        
+printar_ecx_0:          ; Imprimir caractere por caractere (só deve ser usado se a string terminar com um 0 inteiro)
+    printar_loop:
+        mov al, ecx[0]           ; Carregar o caractere atual
+        cmp al, 0               ; Verificar se é o final da string
+        je printar_exit         ; Se for, sair da função
+        mov eax, 4               ; Número da chamada de sistema para imprimir
+        mov ebx, 1               ; Descritor de arquivo (stdout)
+        mov edx, 1               ; Tamanho do caractere a ser impresso
+        int 0x80                 ; Chamar a interrupção do sistema
+        inc ecx                 ; Mover para o próximo caractere
+        jmp printar_loop        ; Repetir o loop
+
+    printar_exit:               ; Saída do loop
+        ret                     ; Retorno
+
 ler_3_2:             ; Ler a string do usuário que terá 3 bytes, mas só serão lidos 2
     mov eax, 3          ; Número da chamada de sistema para ler
     mov ebx, 0          ; Descritor de arquivo (stdin)
